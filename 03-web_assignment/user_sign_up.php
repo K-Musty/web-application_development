e7020a3dbc8470be6dec0229c7268a035cbb117b
K-Musty <kmustapha9564@gmail.com>
2024-02-06 02:32:40 +0100

    Assignment 3

---
+++ 03-web_assignment/user_sign_up.php
@@ -0,0 +1,23 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = ""; //empty string
+$dbname = "Groupp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+
+$sql = "INSERT INTO users (name, email) VALUES ('$name', '$email')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+?>
